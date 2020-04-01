8074fe126d7ba33f1206b60bc58ca037979eba82
yangsaixing <ysxwkk9999@163.com>
2020-04-01 10:21:43 +0800

    测试提交

---
+++ src/main/java/com/family/algorithm/Test.java
@@ -0,0 +1,13 @@
+package com.family.algorithm;
+
+/**
+ * @description:
+ * @author: yangsaixing
+ * @date: Created in 2020/4/1 上午10:21
+ * @modified By:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是个测试类，用来查看git 提交记录");
+    }
+}
