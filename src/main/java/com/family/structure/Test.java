8bcc2d0f8595dd40da1a94c2ca0c5d38da61afde
yangsaixing <ysxwkk9999@163.com>
2021-12-13 14:15:23 +0800

    log4j 测试

--- src/main/java/com/family/structure/Test.java
+++ src/main/java/com/family/structure/Test.java
@@ -1,18 +1,18 @@
 package com.family.structure;
 
 import com.family.structure.tables.Employee;
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.family.structure.tables.Employee.EMPLOYEE;
+import static java.util.logging.Logger.getLogger;
 
 /**
  * @version V1.0
@@ -23,6 +23,9 @@ import static com.family.structure.tables.Employee.EMPLOYEE;
  */
 public class Test {
     public static void main(String[] args) {
+        Logger.getLogger("org.jooq.Constants").setLevel(Level.WARNING);
+
+
         String jdbcUrl = "jdbc:mysql://localhost:3306/family";
         String jdbcUsername = "root";
         String jdbcPassword = "123456";
@@ -39,9 +42,11 @@ public class Test {
             recordResult.forEach(x->{
                 System.out.println(x.getValue(EMPLOYEE.ID));
             });*/
-            List<String> nameList = dslContext.select(EMPLOYEE.NAME).from(EMPLOYEE).fetchInto(String.class);
+           /* List<String> nameList = dslContext.select(EMPLOYEE.NAME).from(EMPLOYEE).fetchInto(String.class);
 
-            nameList.forEach(System.out::println);
+            nameList.forEach(System.out::println);*/
+            Result<Record2<Integer, String>> fetch = dslContext.select(EMPLOYEE.ID,EMPLOYEE.NAME).from(EMPLOYEE)
+                    .orderBy(EMPLOYEE.ID.desc()).limit(1).offset(10).fetch();
         } catch (SQLException e) {
             e.printStackTrace();
         }
